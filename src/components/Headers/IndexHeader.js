eae4fb8b090846cc256016545a645741aa3e778b
Nithin Kumar Moorthy <nithinmoorthy11@gmail.com>
2020-12-16 22:23:32 -0800

    Initial Upload

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,66 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/antoine-barres.jpg") + ")",
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <h1 className="presentation-title">Discover Yourself</h1>
+              <div className="fog-low">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+              <div className="fog-low right">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+            </div>
+            <h2 className="presentation-subtitle text-center">
+              Botox. Microneedling. Hyrdofacials. Schedule your free consultation today.
+            </h2>
+          </Container>
+        </div>
+        <div
+          className="moving-clouds"
+          style={{
+            backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
+          }}
+        />
+        
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
